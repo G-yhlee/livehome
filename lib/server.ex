2266bb447064a8291d0c619795324c2706fd5df2
raetam <raetamslog@gmail.com@gmail.com>
2020-09-09 14:25:13 +0900

    1

--- lib/server.ex
+++ lib/server.ex
@@ -2,7 +2,7 @@ defmodule Server do
   @moduledoc """
   Server keeps the contexts that define your domain
   and business logic.
- 22
+ 222
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
